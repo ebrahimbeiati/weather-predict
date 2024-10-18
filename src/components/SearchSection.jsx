7abed2f432cc0171ecfe2d59a9f00c96b280a7f9
Ebrahim Beiaty <ebrahimbeiaty@gmail.com>
2024-10-18 21:31:00 +0100

    search location

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -12,8 +12,21 @@ const SearchSection = ({getWeatherDetails}) => {
         const API_URL = ` http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${searchInput.value}&days=2`;
         getWeatherDetails(API_URL)
 
-    
-}
+    }
+
+  const handleLocationSearch = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${latitude},${longitude}&days=2`;
+        getWeatherDetails(API_URL)
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  };
+      
   return (
     <div className="search-section">
       <form action="" className="search-form" onSubmit={handleCitySearch}>
@@ -25,7 +38,7 @@ const SearchSection = ({getWeatherDetails}) => {
           className="search-input"
         />
       </form>
-      <button className="location-button">
+      <button className="location-button" onClick={handleLocationSearch}>
         <span className="material-symbols-rounded">my_location</span>{" "}
       </button>
     </div>
